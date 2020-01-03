bec75197320ef8efc5c5bea2305178bf73085ea9
Noam Bonnie <nbonnie@Noams-Caisson-Mac.local>
2020-01-03 15:19:27 -0500

    user request library for clarity

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,10 +1,12 @@
+"use strict";
 const express = require("express");
-const https = require("https");
+const request = require("request-promise-native");
 const app = express();
 const port = 8080;
 
 const CAISSON_API_SERVER = "https://api-noam.caisson.dev";
 
+// Our fake database - a simple map of user_id -> user object
 let users = new Map();
 
 const getUser = id => {
@@ -37,72 +39,57 @@ const exchangeToken = async (req, res) => {
   const user_id = req.body.user_id;
   const caisson_exchange_token = req.body.caisson_exchange_token;
 
-  // Use Caisson's exchangetoken API to exchange it for a permanent check_id
-  const caissonXchgReq = https.request(
-    `${CAISSON_API_SERVER}/v1/idcheck/exchangetoken`,
-    {
-      method: "POST",
-      headers: {
-        Authorization: `Caisson ${process.env.CAISSON_SECRET_API_KEY}`
+  try {
+    // Hit the Caisson token exchange API
+    const caissonXchgRes = await request.post(
+      `${CAISSON_API_SERVER}/v1/idcheck/exchangetoken`,
+      {
+        headers: {
+          // Note the authorization header value is prefixed by 'Caisson '
+          Authorization: `Caisson ${process.env.CAISSON_SECRET_API_KEY}`
+        },
+        body: {
+          // Caisson token exchange API expects the token in JSON format { check_exchange_token: ... }
+          check_exchange_token: caisson_exchange_token
+        },
+        json: true
       }
-    },
-    caissonXchgRes => {
-      let data = "";
+    );
 
-      // A chunk of data has been recieved.
-      caissonXchgRes.on("data", chunk => {
-        data += chunk;
-      });
-
-      // The whole response has been received. Parse the result
-      caissonXchgRes.on("end", async () => {
-        if (caissonXchgRes.statusCode != 200) {
-          // There was an HTTP error. In our example, we'll relay the HTTP error to the user registration web page
-          res.send(
-            "HTTP error during token exchange",
-            caissonXchgRes.statusCode
-          );
-          return;
-        }
-
-        try {
-          const checkData = await JSON.parse(data);
-
-          // Check the response from Caisson for errors.
-          if (checkData.error) {
-            console.error(
-              `Error '${checkData.error}' exchanging Caisson Token: "${checkData.error_message}"`
-            );
-            res.status(500).send("Error exchanging token");
-            return;
-          }
-
-          // All's good. Now we store the Caisson check_id on the user's record
-          let user = getUser(user_id);
-          user.caissonCheckID = checkData.check_id;
-          setUser(user);
-
-          // Echo back the user ID for which we just exchanged the token.
-          res.end(JSON.stringify({ user_id }));
-        } catch (e) {
-          console.error("Failed to parse response", e);
-        }
-      });
+    // Caisson APIs return an error field in the JSON response only in case of errors. Presence of 'error' means
+    // the request failed.
+    if (caissonXchgRes.error) {
+      console.error(
+        `Error '${caissonXchgRes.error}' exchanging Caisson Token: "${caissonXchgRes.error_message}"`
+      );
+      res.status(500).send("Error exchanging token");
+      return;
     }
-  );
 
-  caissonXchgReq.on("error", err => {
-    console.log("Error: " + err.message);
+    // All's good. We exchanged the token for a check_id. Now we store the Caisson check_id on the user's record
+    let user = getUser(user_id);
+    user.caissonCheckID = caissonXchgRes.check_id;
+    setUser(user);
+
+    // Echo back the user ID for which we just exchanged the token.
+    res.end(JSON.stringify({ user_id }));
+  } catch (err) {
+    if (err.statusCode) {
+      // There was an HTTP error. In our example, we'll relay the HTTP error to the user registration web page
+      console.error("Error: " + err.message);
+      res.send("HTTP error during token exchange", err.statusCode);
+      return;
+    }
+
+    // Some other unexpected error occurred.
+    console.error("Error: " + err.message);
     res.status(500).send(`Error exchanging token: ${err}`);
-  });
+  }
 
-  // Issue the call to the Caisson token exchange API
-  caissonXchgReq.write(
-    JSON.stringify({ check_exchange_token: caisson_exchange_token })
-  );
-  caissonXchgReq.end();
+  return;
 };
 
+// Get an ID Check result
 const getIDCheckResult = async (req, res) => {
   if (!req.query || !req.query.user_id) {
     res.status(400).send("invalid input");
@@ -110,7 +97,7 @@ const getIDCheckResult = async (req, res) => {
   }
 
   const user_id = req.query.user_id;
-  let user = getUser(user_id);
+  const user = getUser(user_id);
 
   // If we previously fetched the ID Check data from Caisson, there's no need to fetch it again.
   // Send back the fields we need. We don't want to send back the entire object that contains
@@ -129,92 +116,68 @@ const getIDCheckResult = async (req, res) => {
   }
 
   // We should have exchanged the Check exchanged token for a permanent check ID.
+  // It's an error if we haven't.
   if (!user.caissonCheckID) {
     res.status(400).send("Missing Caisson check ID");
     return;
   }
 
-  // Use Caisson's GET ID Check API to exchange it for a permanent check_id
-  // NOTE: For security reasons we send the Caisson check ID in a header rather
-  // than a query param.
-  const caissonResultReq = https.request(
-    `${CAISSON_API_SERVER}/v1/idcheck`,
-    {
-      method: "GET",
-      headers: {
-        Authorization: `Caisson ${process.env.CAISSON_SECRET_API_KEY}`,
-        "X-Caisson-CheckID": user.caissonCheckID
+  try {
+    // Use Caisson's GET ID Check API to fetch ID Check results
+    const caissonResultRes = await request.get(
+      `${CAISSON_API_SERVER}/v1/idcheck`,
+      {
+        headers: {
+          // Note the authorization header value is prefixed by 'Caisson '
+          Authorization: `Caisson ${process.env.CAISSON_SECRET_API_KEY}`,
+          // For security reasons we send the Caisson check ID in a header rathe than a query param.
+          "X-Caisson-CheckID": user.caissonCheckID
+        },
+        json: true
       }
-    },
-    caissonResultRes => {
-      let data = "";
+    );
 
-      // A chunk of data has been recieved.
-      caissonResultRes.on("data", chunk => {
-        data += chunk;
-      });
-
-      // The whole response has been received. Parse the result
-      caissonResultRes.on("end", async () => {
-        if (caissonResultRes.statusCode != 200) {
-          // There was an HTTP error. In our example, we'll relay the HTTP error to the user registration web page
-          res.send(
-            "HTTP error during token exchange",
-            caissonResultRes.statusCode
-          );
-          return;
-        }
-
-        try {
-          const checkResult = await JSON.parse(data);
-
-          if (checkResult.error == "NOT_VERIFIED") {
-            // The user hasn't verified their ID yet.
-            console.error(`User '${user_id}' has not yet verified their ID`);
-            res
-              .status(500)
-              .send(`User '${user_id}' has not yet verified their ID`);
-            return;
-          } else if (checkResult.error) {
-            // Some other error occurred.
-            console.error(
-              `Error '${checkResult.error}' fetching Caisson ID Check result: "${checkResult.error_message}"`
-            );
-            res.status(500).send("Error fetching ID Check result");
-            return;
-          }
-
-          // All's good. Now we store the Caisson ID Check result on the user's record
-          let user = getUser(user_id);
-          user.idCheckResult = { ...checkResult };
-          setUser(user);
-
-          // Send back the fields we need. We don't want to send back the entire object that contains
-          // sensitive personal data.
-          res.end(
-            JSON.stringify({
-              user_id,
-              id_check_data: {
-                first_name: user.idCheckResult.info.first_name,
-                last_name: user.idCheckResult.info.last_name
-              }
-            })
-          );
-        } catch (e) {
-          console.error("Failed to parse response", e);
-        }
-      });
+    if (caissonResultRes.error == "NOT_VERIFIED") {
+      // The user hasn't verified their ID yet.
+      console.error(`User '${user_id}' has not yet verified their ID`);
+      res.status(500).send(`User '${user_id}' has not yet verified their ID`);
+      return;
+    } else if (caissonResultRes.error) {
+      // Some other error occurred.
+      console.error(
+        `Error '${caissonResultRes.error}' fetching Caisson ID Check result: "${caissonResultRes.error_message}"`
+      );
+      res.status(500).send("Error fetching ID Check result");
+      return;
     }
-  );
 
-  caissonResultReq.on("error", err => {
-    console.log("Error: " + err.message);
-    res.status(500).send(`Error exchanging token: ${err}`);
-  });
+    // All's good. Now we store the Caisson ID Check result on the user's record
+    user.idCheckResult = { ...caissonResultRes };
+    setUser(user);
 
-  // Issue the call to the Caisson token exchange API
-  // caissonResultReq.write();
-  caissonResultReq.end();
+    // Send the fields we need back to the web page. We don't want to send back the entire
+    // object that contains sensitive personal data.
+    res.end(
+      JSON.stringify({
+        user_id,
+        id_check_data: {
+          first_name: user.idCheckResult.info.first_name,
+          last_name: user.idCheckResult.info.last_name
+        }
+      })
+    );
+  } catch (err) {
+    if (err.statusCode) {
+      // There was an HTTP error. In our example, we'll relay the HTTP error to the user registration web page
+      console.error("Error: " + err.message);
+      res.send("HTTP error during fetching ID Check result", err.statusCode);
+      return;
+    }
+
+    // Some other unexpected error occurred.
+    console.error("Error: " + err.message);
+    res.status(500).send(`Error fetching ID Check: ${err}`);
+  }
 };
 
 const startServer = async () => {
